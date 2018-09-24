87feccbd20b7205fc4ce6cc71360225958912437
peterleist <peterle606@gmail.com>
2018-09-24 17:25:36 +0200

    Start_dev

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+
+
+public class Main{
+    public static void main(String[] args){
+        Node test = new Node("Test");
+        System.out.println(test.GetData());
+    }
+}
---
+++ Node.java
@@ -0,0 +1,9 @@
+public class Node{
+    private String node_data;
+
+    public Node(String data){ node_data = data; }
+
+    public void SetData(String data){ node_data = data;}
+    public String GetData(){ return node_data; } 
+
+}
